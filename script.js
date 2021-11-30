1775e7c6e05a98bd4e49a972d3ab29c810f70a37
kirill3518 <kirill3518@mail.ru>
2021-12-01 02:29:28 +0500

    ДЗ № 3.

--- script.js
+++ script.js
@@ -1,61 +1,48 @@
-const goods = [
-  { image: "images/featured/1.jpg", title: 'Shirt', price: 150 },
-  { image: "images/featured/2.jpg", title: 'Socks', price: 50 },
-  { image: "images/featured/3.jpg", title: 'Jacket', price: 350 },
-  { image: "images/featured/4.jpg", title: 'Shoes', price: 250 },
-];
+const reformData = (items) => {
+  return items.map(({ product_name, ...rest }) => {
+    return {
+      ...rest,
+      title: product_name
+    }
+  });
+}
+
+const URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
+const GOODS_POSTFIX = "/catalogData.json";
+
+const service = function (url, postfix) {
+  return new Promise((resolve, reject) => {
+    fetch(`${url}${postfix}`).then((res) => {
+      return res.json();
+    }).then((data) => {
+      resolve(data);
+    });
+  });
+}
 
 class GoodsItem {
-  constructor({ image, title, price }) {
-    this.image = image;
+  constructor({ title, price }) {
     this.title = title;
     this.price = price;
   }
 
   render() {
     return `
-    <div class="featuredItem" data-id="1" data-name="ELLERY X M'O CAPSULE 1" data-price="52.22">
-      <div class="featuredImgWrap">
-        <img src=${this.image} alt="">
-        <div class="featuredImgDark">
-          <button class="addToCart">
-            <img src="images/cart.svg" alt="">
-            Add to Cart
-          </button>
-        </div>
-      </div>
+    <div class="featuredItem">
       <div class="featuredData">
         <div class="featuredName">
           ${this.title}
         </div>
-        <div class="featuredText">
-          Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery
-          teams
-          up with Moda Operandi.
-        </div>
         <div class="featuredPrice">
           $${this.price}
         </div>
-      </div>
-    </div>   
+      </div >
+    </div >
   `;
   }
 }
 
 class GoodsList {
-  constructor() {
-    this.goods = goods;
-  }
-
-  render() {
-    const _goods = [...this.goods];
-
-    const _goodsItems = _goods.map((item) => {
-      const goodsItem = new GoodsItem(item);
-      return goodsItem.render();
-    });
-    document.querySelector('.goods-list .featuredItems').innerHTML = _goodsItems.join('');
-  }
 
   getTotalSum() {
     const _goods2 = [...this.goods];
@@ -66,57 +53,84 @@ class GoodsList {
     return goodsPrice;
   }
 
+  setGoods() {
+    return service(URL, GOODS_POSTFIX).then((data) => {
+      return reformData(data);
+    });
+  }
+
+  render() {
+    this.setGoods().then((data) => {
+      this.goods = data;
+      const _goods = [...this.goods];
+      const _goodsItems = _goods.map((item) => {
+        const goodsItem = new GoodsItem(item);
+        return goodsItem.render();
+      });
+      document.querySelector('.goods-list .featuredItems').innerHTML = _goodsItems.join('');
+    });
+  }
+}
+
+
+const productEl = document.querySelector('.products');
+const cartIconWrapEl = document.querySelector('.cartIconWrap');
+const spanEl = cartIconWrapEl.querySelector("span");
+const basketTotalValueEl = document.querySelector('.basketTotalValue');
+const basketEl = document.querySelector('.basket');
+cartIconWrapEl.onclick = () => {
+  basketEl.classList.toggle("hidden");
+}
+
+
+const CART_POSTFIX = "/getBasket.json";
+
+class Basket {
+
+  setBasket() {
+    return service(URL, CART_POSTFIX);
+  }
+
+  render() {
+    this.setBasket().then((data) => {
+      this.goods = data;
+      const { contents, amount, countGoods } = this.goods;
+      const _basketItems = contents.map((item) => {
+        const backetItem = new BasketItem(item);
+        return backetItem.render();
+      });
+      productEl.innerHTML = _basketItems.join('');
+      spanEl.textContent = countGoods;
+      basketTotalValueEl.textContent = amount;
+    });
+  }
+}
+
+class BasketItem {
+
+  constructor({ product_name, quantity, price }) {
+    this.product_name = product_name;
+    this.quantity = quantity;
+    this.price = price;
+  }
+
+  render() {
+    return `
+    <div class="product">
+      <div>${this.product_name}</div>
+      <div>${this.quantity} шт.</div>
+      <div>$${this.price}</div>
+      <div>$${(this.quantity * this.price).toFixed(2)}</div>
+		</div>
+  `;
+  }
 }
 
 onload = () => {
   const goodsList = new GoodsList();
   goodsList.render();
+
+  const basket = new Basket();
+  basket.render();
 }
 
-class Basket {
-
-  open() {
-
-  }
-
-  close() {
-
-  }
-
-  addItem() {
-
-  }
-
-  removeItem() {
-
-  }
-
-  getTotalSum() {
-
-  }
-
-  getTotalCount() {
-
-  }
-
-}
-
-class BasketItem {
-
-  addCount() {
-
-  }
-
-  removeCount() {
-
-  }
-
-  getSum() {
-
-  }
-
-  getCount() {
-
-  }
-
-}
